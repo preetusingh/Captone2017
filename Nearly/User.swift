988e5d0bfe3bcd1a4c334f5d3a5a5c0d9b8931e5
Dhara's Mac <dji@Akshats-MacBook-Pro.local>
2017-06-06 19:23:55 -0700

    First

---
+++ Nearly/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Nearly
+//
+//  Created by Dhara's Mac on 6/6/17.
+//  Copyright © 2017 Nearly. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
---
+++ Nearly/loginInputView.swift
@@ -0,0 +1,21 @@
+//
+//  loginInputView.swift
+//  Nearly
+//
+//  Created by Dhara's Mac on 6/6/17.
+//  Copyright © 2017 Nearly. All rights reserved.
+//
+
+import UIKit
+
+class loginInputView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
