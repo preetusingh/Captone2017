a42a26d8f23e1ed3e26e55777f05df0bdde77a33
Dhara's Mac <dharabavishi@gmail.com>
2017-06-13 14:57:21 -0700

    added constant file

---
+++ Nearly/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  Nearly
+//
+//  Created by Dhara's Mac on 6/13/17.
+//  Copyright © 2017 Nearly. All rights reserved.
+//
+
+import Foundation
+
+//Color Constant
+let kXHERYellow = UIColor(red: 250/255, green: 225/255, blue: 30/255, alpha: 1)
+let kXHEROrange = UIColor(red: 255/255, green: 140/255, blue: 0/255, alpha: 1)
